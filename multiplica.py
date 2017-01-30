93b6b339927a5b772d5ce4e227192a29e6c51afc
Laura Trabas Clavero <Laura@MacBook-Pro-de-Laura-2.local>
2017-01-30 17:07:34 +0100

    Entrega del ejercicio 13.3

---
+++ multiplica.py
@@ -0,0 +1,5 @@
+for num1 in range(1,11):
+    print("\nLa tabla de multiplicar del", num1, "es:")
+    for num2 in range(1,11):
+        resultado = num1 * num2
+        print(num1, "*", num2, "=", resultado)
